0bec39c332a6515ba560c8da0b3b218eb9cd0c64
TudorBLD <tbaldean@yahoo.ro>
2022-08-03 16:41:39 +0300

    Add a description to the test code

--- Test_code/Test_code.ino
+++ Test_code/Test_code.ino
@@ -1,3 +1,9 @@
+/*
+This is a code for testing the output functionality of arduino pins.
+If all used pins are connected to leds, the leds will light up one by one.
+*/
+
+
 int HW_P[] = {2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, A0, A1, A2, A3, A4, A5, A6, A7};
 
 
