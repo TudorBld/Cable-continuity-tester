f9710f1793b69e489001f47e9fc176ec3538895c
TudorBLD <tbaldean@yahoo.ro>
2022-09-21 18:38:14 +0300

    Begin work on lcd display code

--- code/code.ino
+++ code/code.ino
@@ -6,10 +6,12 @@
 
 #define RED_BRIGHTNESS 70
 
-#include <SoftPWM.h>
+#include <SoftPWM.h>        //by Brett Hagman
+#include <LiquidCrystal_I2C.h>
 
 //Declare global variables
 int reset_happened = 0;
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // available pins in this array
 int HW_P[] = {2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, A0, A1, A2, A3, A4, A5, A6, A7};
